87c6cd232accd3719a8bd632abaae7b197c615ac
YoniA <yoniasulin@gmail.com>
2024-07-17 21:43:54 +0300

    Add link mode functionality

--- vim-scroll.js
+++ vim-scroll.js
@@ -1,8 +1,24 @@
 let lastKey = null;
 
+// link mode state
+let linkMode = false;
+let linkHash = {}
+let linkModeBuffer = '';
+
 document.addEventListener('keydown', (e) => {
 	const SCROLL_STEP = 50;
 	const keyName = e.key;
+
+  if (linkMode) {
+    linkModeBuffer += keyName;
+    setTimeout(() => {
+      const url = linkHash[linkModeBuffer];
+      if (url && url.length > 0) {
+        window.location.href = url;
+        resetLinkModeStat();
+      }
+    }, 1000);
+  }
 
 	switch (keyName) {
 		case 'j':
@@ -38,6 +54,19 @@ document.addEventListener('keydown', (e) => {
 		case 'L':
 			history.forward();
 			break;
+    case 'f':
+      if (!linkMode) {
+        linkMode = true;
+        lableAllLinks();
+      }
+      break;
+    case 'Escape':
+      if(linkMode != false) {
+        linkMode = false;
+        hideAllLinkLabels();
+        resetLinkModeState();
+      }
+      break;
 	}
 
 	lastKey = {
@@ -46,3 +75,36 @@ document.addEventListener('keydown', (e) => {
 	};
 
 });
+
+
+
+function lableAllLinks() {
+    const links = document.querySelector('a');
+
+    Array.from(document.getElementsByTagName('a')).forEach((link, index) => {
+          const url = link.href;
+          linkHash[index] = url;
+
+        
+          
+          const label = document.createElement('span');
+          label.classList.add(`link-label`);
+          label.textContent = `${index}`;
+          label.style='background-color: yellow; border: 1px solid black; border-radius: 5px';
+          link.parentNode.insertBefore(label, link.nextSibling);
+          
+        });
+
+    console.table(linkHash);
+}
+
+
+function hideAllLinkLabels() {
+  Array.from(document.getElementsByClassName('link-label')).forEach(label => label.style = 'display: none');
+}
+
+
+function resetLinkModeState() {
+  linkModeBuffer = '';
+  linkHash = {};
+}
